d2cd0a320085823043fae27c6ff3c15db45d8870
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2023-11-06 12:00:03 +0530

    Add files via upload

---
+++ internl.php
@@ -0,0 +1,87 @@
+<html>
+    <style>
+        th{
+            font-size:18px;
+        }
+        td{
+            height:40;
+        }
+    </style>
+    <body>
+    <center>
+        <br><br>
+        <form id="form1" method='post' action="intout.php">
+            <h1> TRINITY COLLEGE OF LONDON </h1>
+            <h1> Master of Computer Applications</h1>
+            <h2> INTERNAL MARKS </h2>
+            <h2> Semester One </h2>
+            <table border="1" width="1" cellpadding="5">
+                <thead>
+                    <tr>
+                        <th rowspan="2"><h3>SL:NO</h3></th>
+                        <th rowspan="2"><h3>NAME</h3></th>
+                        <th rowspan="2"><h3>ROLL NO</h3></th>
+                        <th colspan="4"><h3>INTERNAL MARK</h3></th>
+                    </tr>
+                    <tr>
+                        <th><h3>MFC</h3>out of 20</th>
+                        <th><h3>ASE</h3>out of 20</th>
+                        <th><h3>DFCS</h3>out of 20</th>
+                        <th><h3>ADS</h3>out of 20</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td><input type="text" name="name[0]"></td>
+                        <td><input type="text" name="roll[0]" size="4"></td>
+                        <td><input type="text" name="MFC[0]" size="4"></td>
+                        <td><input type="text" name="ASE[0]" size="4"></td>
+                        <td><input type="text" name="DFCS[0]" size="4"></td>
+                        <td><input type="text" name="ADS[0]" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td><input type="text" name="name[1]"></td>
+                        <td><input type="text" name="roll[1]" size="4"></td>
+                        <td><input type="text" name="MFC[1]" size="4"></td>
+                        <td><input type="text" name="ASE[1]" size="4"></td>
+                        <td><input type="text" name="DFCS[1]" size="4"></td>
+                        <td><input type="text" name="ADS[1]" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td><input type="text" name="name[2]"></td>
+                        <td><input type="text" name="roll[2]" size="4"></td>
+                        <td><input type="text" name="MFC[2]" size="4"></td>
+                        <td><input type="text" name="ASE[2]" size="4"></td>
+                        <td><input type="text" name="DFCS[2]" size="4"></td>
+                        <td><input type="text" name="ADS[2]" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td><input type="text" name="name[3]"></td>
+                        <td><input type="text" name="roll[3]" size="4"></td>
+                        <td><input type="text" name="MFC[3]" size="4"></td>
+                        <td><input type="text" name="ASE[3]" size="4"></td>
+                        <td><input type="text" name="DFCS[3]" size="4"></td>
+                        <td><input type="text" name="ADS[3]" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td><input type="text" name="name[4]"></td>
+                        <td><input type="text" name="roll[4]" size="4"></td>
+                        <td><input type="text" name="MFC[4]" size="4"></td>
+                        <td><input type="text" name="ASE[4]" size="4"></td>
+                        <td><input type="text" name="DFCS[4]" size="4"></td>
+                        <td><input type="text" name="ADS[4]" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="7"><center><input type="submit" name="submit" value="submit">&nbsp;&nbsp;&nbsp;&nbsp;<input type="reset" name="reset" value="reset"></center></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </center>
+    </body>
+</html>
