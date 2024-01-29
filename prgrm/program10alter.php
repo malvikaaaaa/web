4ab39463fd44e852e849dce2a5e94ca9bfb02133
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-01-29 12:33:21 +0530

    Update program10alter.php

--- prgrm/program10alter.php
+++ prgrm/program10alter.php
@@ -1,7 +1,15 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+$con=mysqli_connect('localhost','root','','product');
+if($con->connect_error)
+    echo "Connection Failed";
+else
+    echo "Connected Successfully";
 
+$sql='alter table tbl_product add pro_manufacture varchar(20)';
+    if(mysqli_query($con,$sql))
+        echo "Column Added Successfully";
+    else
+        echo "Failed";
+
+?>
